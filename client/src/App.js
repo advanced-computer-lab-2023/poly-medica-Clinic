70c7f0e198870ba97fd3360beee45c68d735f3df
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2023-10-06 08:19:36 +0200

    add frontend directories

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import { useSelector } from 'react-redux';
+
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
+
+// routing
+import Routes from 'routes';
+
+// defaultTheme
+import themes from 'themes';
+
+
+// ==============================|| APP ||============================== //
+
+const App = () => {
+  const customization = useSelector((state) => state.customization);
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={themes(customization)}>
+        <CssBaseline />
+          <Routes />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
+};
+
+export default App;
---
+++ client/src/pages/utilities/Color.js
@@ -0,0 +1,11 @@
+import MainCard from 'ui-component/cards/MainCard';
+import SecondaryAction from 'ui-component/cards/CardSecondaryAction';
+
+
+const UIColor = () => (
+  <MainCard title="Color Palette" secondary={<SecondaryAction link="https://next.material-ui.com/system/palette/" />}>
+    
+  </MainCard>
+);
+
+export default UIColor;
