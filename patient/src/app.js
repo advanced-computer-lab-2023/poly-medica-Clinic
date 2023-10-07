237dbfdaeee84dff90d1f7dc920831e7ab17db16
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2023-10-08 00:32:21 +0200

    add axios and cors

--- patient/src/app.js
+++ patient/src/app.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { patient } from './api/patient.js';
 import { PORT_NUMBER } from './utils/Constants.js';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
@@ -11,21 +12,25 @@ const mongoURL = process.env.MONGO_URI;
 
 const connect = async () => {
 	try {
-		await mongoose.connect( mongoURL );
-		console.log( 'Database connected' );
-	} catch ( err ) {
-		console.error( 'Error connecting to the database:', err );
+		await mongoose.connect(mongoURL);
+		console.log('Database connected');
+	} catch (err) {
+		console.error('Error connecting to the database:', err);
 	}
 };
 
 await connect();
 
-app.use( express.json() );
+app.use(express.json());
+app.use(cors({
+	origin: ["http://localhost:3000","http://localhost:3001", "http://localhost:3002"],
+	credentials: true
+}))
 
-patient( app );
+patient(app);
 
 const port = process.env.PORT || PORT_NUMBER;
 
-app.listen( port, () => {
-	console.log( `Server is running on port ${port}` );
-} );
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
