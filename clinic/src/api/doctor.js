dbf5ceed39285e0e102a4eb94d7d063e089cad12
Abdelrahman Elmeky <65960126+Aelmeky@users.noreply.github.com>
2023-10-07 19:25:06 +0300

    style : removing unused variables

--- clinic/src/api/doctor.js
+++ clinic/src/api/doctor.js
@@ -1,7 +1,7 @@
 //import DoctorService from '../service/doctor-service.js';
 //import { EMPTY_SIZE, ERROR_STATUS_CODE, NOT_FOUND_STATUS_CODE, OK_STATUS_CODE } from '../utils/Constants.js';
 
-export const doctor = (app) => {
-	//const service = new DoctorService();
+// export const doctor = (app) => {
+ 	//const service = new DoctorService();
     
-}
+// }
