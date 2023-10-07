0dfa048bd5ef7563128fe4d6b5adb64b5272f937
Mohamed Khaled <113786472+Mohamed-Khaled308@users.noreply.github.com>
2023-10-07 18:24:36 +0300

    apply eslint rules

--- patient/src/database/repository/patient-repository.js
+++ patient/src/database/repository/patient-repository.js
@@ -2,17 +2,17 @@ import PatientModel from '../models/Patient.js';
 import { FAMILY_MEMBERS_PROJECTION } from '../../utils/Constants.js';
 
 class PatientRepository {
-    async findAllPatients() {
-        const allPatients = await PatientModel.find();
-        return allPatients;
-    }
-    async findFamilyMembers(id) {
-        const familyMembers = await PatientModel.findById(
-            id,
-            FAMILY_MEMBERS_PROJECTION
-        );
-        return familyMembers;
-    }
+	async findAllPatients() {
+		const allPatients = await PatientModel.find();
+		return allPatients;
+	}
+	async findFamilyMembers(id) {
+		const familyMembers = await PatientModel.findById(
+			id,
+			FAMILY_MEMBERS_PROJECTION
+		);
+		return familyMembers;
+	}
 }
 
 export default PatientRepository;
