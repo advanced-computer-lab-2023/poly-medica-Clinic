add71301ca43027859f2e2f55beda35be7d9caf7
AhmadHoseiny <ahmadshahhat9898@gmail.com>
2023-10-08 13:55:37 +0300

    fix: adding try catch

--- patient/src/api/prescription.js
+++ patient/src/api/prescription.js
@@ -6,23 +6,33 @@ export const prescription = ( app ) => {
 	const service = new PrescriptionService();
     
 	app.get('/prescriptions', async (req, res) => {
-		const prescriptions = await service.getAllPrescriptions();
-		if (prescriptions.length > EMPTY_SIZE) {
-			res.status(OK_STATUS_CODE).json(prescriptions);
-		} else {
-			res.status(NOT_FOUND_STATUS_CODE).json({ message: 'prescriptions not found' });
+		try{
+			const prescriptions = await service.getAllPrescriptions();
+			if (prescriptions.length > EMPTY_SIZE) {
+				res.status(OK_STATUS_CODE).json(prescriptions);
+			} else {
+				res.status(NOT_FOUND_STATUS_CODE).json({ message: 'prescriptions not found' });
+			}
+		}
+		catch(error){
+			res.status(ERROR_STATUS_CODE).json({ message: error });
 		}
 	});
 
 	app.get('/prescription/:id', async (req, res) => {
-		const id = req.params.id;
-		if(!isValidMongoId(id))
-			return res.status(ERROR_STATUS_CODE).json({ message:'Invalid ID' });
-		const prescription = await service.getPrescriptionById(id);
-		if (prescription) {
-			res.status(OK_STATUS_CODE).json(prescription);
-		} else {
-			res.status(NOT_FOUND_STATUS_CODE).json({ message: 'prescription not found' });
+		try{
+			const id = req.params.id;
+			if(!isValidMongoId(id))
+				return res.status(ERROR_STATUS_CODE).json({ message:'Invalid ID' });
+			const prescription = await service.getPrescriptionById(id);
+			if (prescription) {
+				res.status(OK_STATUS_CODE).json(prescription);
+			} else {
+				res.status(NOT_FOUND_STATUS_CODE).json({ message: 'prescription not found' });
+			}
+		}
+		catch(error){
+			res.status(ERROR_STATUS_CODE).json({ message: error });
 		}
 	});
 
