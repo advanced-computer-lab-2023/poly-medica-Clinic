905acc0e8d0037f3cfe090ad4cd483dfdf432274
Mohamed Khaled <113786472+Mohamed-Khaled308@users.noreply.github.com>
2023-10-08 12:26:02 +0300

    feat: adding get appointments by user id

--- clinic/src/app.js
+++ clinic/src/app.js
@@ -5,9 +5,8 @@ import { healthPackage } from './api/HealthPackageAPI.js';
 import { PORT } from './utils/Constants.js';
 import cors from 'cors';
 //import {doctor } from './api/doctor.js';
-//import {appointment } from './api/appointment.js';
+import { appointment } from './api/AppointmentAPI.js';
 //import {admin } from './api/admin.js';
-
 
 dotenv.config();
 const app = express();
@@ -26,15 +25,21 @@ const connect = async () => {
 await connect();
 
 app.use(express.json());
-app.use(cors({
-	origin: ["http://localhost:3000","http://localhost:3001", "http://localhost:3002"], 
-	credentials: true
-  }))
+app.use(
+	cors({
+		origin: [
+			'http://localhost:3000',
+			'http://localhost:3001',
+			'http://localhost:3002',
+		],
+		credentials: true,
+	})
+);
 
 healthPackage(app);
 //admin(app);
 //doctor(app);
-//appointment(app);
+appointment(app);
 
 const port = process.env.PORT || PORT;
 
