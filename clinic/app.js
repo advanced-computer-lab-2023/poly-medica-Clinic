2319d5161f5b11446482b4fe3cae484a804d774e
amir-awad <amirawad004@gmail.com>
2023-10-12 19:14:13 +0300

    merged from main

--- clinic/app.js
+++ clinic/app.js
@@ -15,26 +15,26 @@ const mongoURL = process.env.MONGO_URI;
 console.log(mongoURL);
 
 const connect = async () => {
-    try {
-        await mongoose.connect(mongoURL);
-        console.log('Database connected');
-    } catch (err) {
-        console.error('Error connecting to the database:', err.message);
-    }
+	try {
+		await mongoose.connect(mongoURL);
+		console.log('Database connected');
+	} catch (err) {
+		console.error('Error connecting to the database:', err.message);
+	}
 };
 
 await connect();
 
 app.use(express.json());
 app.use(
-    cors({
-        origin: [
-            'http://localhost:3000',
-            'http://localhost:3001',
-            'http://localhost:3002',
-        ],
-        credentials: true,
-    })
+	cors({
+		origin: [
+			'http://localhost:3000',
+			'http://localhost:3001',
+			'http://localhost:3002',
+		],
+		credentials: true,
+	})
 );
 
 healthPackage(app);
@@ -45,5 +45,5 @@ appointment(app);
 const port = process.env.PORT || PORT;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
--- patient/src/database/repository/patient-repository.js
+++ patient/src/database/repository/patient-repository.js
@@ -3,49 +3,49 @@ import PrescriptionModel from '../models/Prescription.js';
 import { FAMILY_MEMBERS_PROJECTION } from '../../utils/Constants.js';
 
 class PatientRepository {
-    async findAllPatients() {
-        const allPatients = await PatientModel.find();
-        return allPatients;
-    }
-    async findFamilyMembers(id) {
-        const familyMembers = await PatientModel.findById(
-            id,
-            FAMILY_MEMBERS_PROJECTION
-        );
-        return familyMembers;
-    }
+	async findAllPatients() {
+		const allPatients = await PatientModel.find();
+		return allPatients;
+	}
+	async findFamilyMembers(id) {
+		const familyMembers = await PatientModel.findById(
+			id,
+			FAMILY_MEMBERS_PROJECTION
+		);
+		return familyMembers;
+	}
 
-    async findPatientByUserName(userN) {
-        return await PatientModel.findOne({ userName: userN });
-    }
+	async findPatientByUserName(userN) {
+		return await PatientModel.findOne({ userName: userN });
+	}
 
-    async addFamilyMember(id, familyMembers) {
-        return await PatientModel.findOneAndUpdate(
-            { _id: id },
-            { familyMembers },
-            { new: true, runValidators: true }
-        ).select(FAMILY_MEMBERS_PROJECTION);
-    }
+	async addFamilyMember(id, familyMembers) {
+		return await PatientModel.findOneAndUpdate(
+			{ _id: id },
+			{ familyMembers },
+			{ new: true, runValidators: true }
+		).select(FAMILY_MEMBERS_PROJECTION);
+	}
 
-    async findAllPrescriptions() {
-        const allPrescriptions = await PrescriptionModel.find();
-        return allPrescriptions;
-    }
+	async findAllPrescriptions() {
+		const allPrescriptions = await PrescriptionModel.find();
+		return allPrescriptions;
+	}
 
-    async findPrescriptionById(id) {
-        const prescription = await PrescriptionModel.findById(id);
-        return prescription;
-    }
+	async findPrescriptionById(id) {
+		const prescription = await PrescriptionModel.findById(id);
+		return prescription;
+	}
 
-    async createPatient(patient) {
-        const newPatient = await PatientModel.create(patient);
-        return newPatient;
-    }
+	async createPatient(patient) {
+		const newPatient = await PatientModel.create(patient);
+		return newPatient;
+	}
 
-    async deletePatient(id) {
-        const deletedPatient = await PatientModel.findByIdAndDelete(id);
-        return deletedPatient;
-    }
+	async deletePatient(id) {
+		const deletedPatient = await PatientModel.findByIdAndDelete(id);
+		return deletedPatient;
+	}
 }
 
 export default PatientRepository;
