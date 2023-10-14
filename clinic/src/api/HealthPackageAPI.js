5c40c9716ff75823c367f4b1daff95a6822c8632
Mohamed Khaled <113786472+Mohamed-Khaled308@users.noreply.github.com>
2023-10-14 18:50:48 +0300

    fix style errors

--- clinic/src/api/HealthPackageAPI.js
+++ clinic/src/api/HealthPackageAPI.js
@@ -1,79 +1,78 @@
 import HealthPackageService from '../service/health-package-service.js';
 import {
-    EMPTY_SIZE,
-    ERROR_STATUS_CODE,
-    NOT_FOUND_STATUS_CODE,
-    OK_STATUS_CODE,
+	ERROR_STATUS_CODE,
+	NOT_FOUND_STATUS_CODE,
+	OK_STATUS_CODE,
 } from '../utils/Constants.js';
 import { isValidMongoId } from '../utils/Validation.js';
 
 export const healthPackage = (app) => {
-    const service = new HealthPackageService();
+	const service = new HealthPackageService();
 
-    app.get('/packages', async (req, res) => {
-        const allPackages = await service.getAllPackages();
-        if (allPackages) {
-            res.status(OK_STATUS_CODE).json({ allPackages });
-        } else {
-            res.status(NOT_FOUND_STATUS_CODE).json({
-                message: 'packages not found',
-            });
-        }
-    });
+	app.get('/packages', async (req, res) => {
+		const allPackages = await service.getAllPackages();
+		if (allPackages) {
+			res.status(OK_STATUS_CODE).json({ allPackages });
+		} else {
+			res.status(NOT_FOUND_STATUS_CODE).json({
+				message: 'packages not found',
+			});
+		}
+	});
 
-    app.post('/packages', async (req, res) => {
-        const newPackage = req.body.newPackage;
-        const name = newPackage.name;
-        const price = Number(newPackage.price);
-        const discountOfDoctor = Number(newPackage.discountOfDoctor);
-        const discountOfMedicin = Number(newPackage.discountOfMedicin);
-        const discountOfFamily = Number(newPackage.discountOfFamily);
+	app.post('/packages', async (req, res) => {
+		const newPackage = req.body.newPackage;
+		const name = newPackage.name;
+		const price = Number(newPackage.price);
+		const discountOfDoctor = Number(newPackage.discountOfDoctor);
+		const discountOfMedicin = Number(newPackage.discountOfMedicin);
+		const discountOfFamily = Number(newPackage.discountOfFamily);
 
-        try {
-            const data = await service.createNewPackage(
-                name,
-                price,
-                discountOfDoctor,
-                discountOfMedicin,
-                discountOfFamily
-            );
-            if (data) {
-                res.status(OK_STATUS_CODE).json({ data });
-            } else {
-                res.status(NOT_FOUND_STATUS_CODE);
-            }
-        } catch (err) {
-            res.status(ERROR_STATUS_CODE).json({ err: err.message });
-        }
-    });
+		try {
+			const data = await service.createNewPackage(
+				name,
+				price,
+				discountOfDoctor,
+				discountOfMedicin,
+				discountOfFamily
+			);
+			if (data) {
+				res.status(OK_STATUS_CODE).json({ data });
+			} else {
+				res.status(NOT_FOUND_STATUS_CODE);
+			}
+		} catch (err) {
+			res.status(ERROR_STATUS_CODE).json({ err: err.message });
+		}
+	});
 
-    app.patch('/package/:id', async (req, res) => {
-        const { selectedEditPackages } = req.body;
-        const id = req.params.id;
-        if (!isValidMongoId(id))
-            return res
-                .status(ERROR_STATUS_CODE)
-                .json({ message: 'Invalid ID' });
-        try {
-            console.log(selectedEditPackages);
-            const updatedPackage = await service.updatePackage(
-                id,
-                selectedEditPackages
-            );
-            res.status(OK_STATUS_CODE).json({ updatedPackage });
-        } catch (err) {
-            res.status(ERROR_STATUS_CODE).json({ err: err.message });
-        }
-    });
+	app.patch('/package/:id', async (req, res) => {
+		const { selectedEditPackages } = req.body;
+		const id = req.params.id;
+		if (!isValidMongoId(id))
+			return res
+				.status(ERROR_STATUS_CODE)
+				.json({ message: 'Invalid ID' });
+		try {
+			console.log(selectedEditPackages);
+			const updatedPackage = await service.updatePackage(
+				id,
+				selectedEditPackages
+			);
+			res.status(OK_STATUS_CODE).json({ updatedPackage });
+		} catch (err) {
+			res.status(ERROR_STATUS_CODE).json({ err: err.message });
+		}
+	});
 
-    app.delete('/packages/:id', async (req, res) => {
-        const id = req.params.id;
-        try {
-            console.log(id);
-            const deletedPackage = await service.deletePackage(id);
-            res.status(OK_STATUS_CODE).json({ deletedPackage });
-        } catch (err) {
-            res.status(ERROR_STATUS_CODE).json({ err: err.message });
-        }
-    });
+	app.delete('/packages/:id', async (req, res) => {
+		const id = req.params.id;
+		try {
+			console.log(id);
+			const deletedPackage = await service.deletePackage(id);
+			res.status(OK_STATUS_CODE).json({ deletedPackage });
+		} catch (err) {
+			res.status(ERROR_STATUS_CODE).json({ err: err.message });
+		}
+	});
 };
--- clinic/src/service/doctor-service.js
+++ clinic/src/service/doctor-service.js
@@ -1,67 +1,66 @@
 import DoctorRepository from '../database/repository/doctor-repository.js';
-import { EMPTY_SIZE } from '../utils/Constants.js';
 
 class DoctorService {
-    constructor() {
-        this.repository = new DoctorRepository();
-    }
+	constructor() {
+		this.repository = new DoctorRepository();
+	}
 
-    async findAllDoctorRequests() {
-        const doctorRequests = await this.repository.findAllDoctorRequests();
-        return doctorRequests;
-    }
+	async findAllDoctorRequests() {
+		const doctorRequests = await this.repository.findAllDoctorRequests();
+		return doctorRequests;
+	}
 
-    async addDoctor(req) {
-        const doctorUser = await this.repository.addDoctor(req);
-        return doctorUser;
-    }
+	async addDoctor(req) {
+		const doctorUser = await this.repository.addDoctor(req);
+		return doctorUser;
+	}
 
-    async addReqDoctor(req) {
-        const doctorUser = await this.repository.addDoctorReq(req);
-        return doctorUser;
-    }
+	async addReqDoctor(req) {
+		const doctorUser = await this.repository.addDoctorReq(req);
+		return doctorUser;
+	}
 
-    async deleteDoctorRequest(id) {
-        const doctorRequest = await this.repository.deleteDoctorRequest(id);
-        return doctorRequest;
-    }
+	async deleteDoctorRequest(id) {
+		const doctorRequest = await this.repository.deleteDoctorRequest(id);
+		return doctorRequest;
+	}
 
-    async findAllDoctors() {
-        const doctors = await this.repository.findAllDoctors();
-        return doctors;
-    }
+	async findAllDoctors() {
+		const doctors = await this.repository.findAllDoctors();
+		return doctors;
+	}
 
-    async getDoctorById(id) {
-        const doctor = await this.repository.findDoctorById(id);
-        return doctor;
-    }
+	async getDoctorById(id) {
+		const doctor = await this.repository.findDoctorById(id);
+		return doctor;
+	}
 
-    async createDoctor(doctor) {
-        const newDoctor = await this.repository.createDoctor(doctor);
-        return newDoctor;
-    }
+	async createDoctor(doctor) {
+		const newDoctor = await this.repository.createDoctor(doctor);
+		return newDoctor;
+	}
 
-    async deleteDoctor(id) {
-        const deletedDoctor = await this.repository.deleteDoctor(id);
-        return deletedDoctor;
-    }
+	async deleteDoctor(id) {
+		const deletedDoctor = await this.repository.deleteDoctor(id);
+		return deletedDoctor;
+	}
 
-    async findAllPatients(id) {
-        const allPatients = await this.repository.findAllPatients(id);
-        return allPatients;
-    }
-    async getAllAppointments() {
-        const allAppointments = await this.repository.findAllAppointments();
-        return allAppointments;
-    }
-    async updateDoctor(id, updates) {
-        const doctor = await this.repository.updateDoctor(id, updates);
-        return doctor;
-    }
+	async findAllPatients(id) {
+		const allPatients = await this.repository.findAllPatients(id);
+		return allPatients;
+	}
+	async getAllAppointments() {
+		const allAppointments = await this.repository.findAllAppointments();
+		return allAppointments;
+	}
+	async updateDoctor(id, updates) {
+		const doctor = await this.repository.updateDoctor(id, updates);
+		return doctor;
+	}
 
-    async checkDoctorReqUser(req) {
-        await this.repository.checkDoctorReqUser(req);
-    }
+	async checkDoctorReqUser(req) {
+		await this.repository.checkDoctorReqUser(req);
+	}
 }
 
 export default DoctorService;
