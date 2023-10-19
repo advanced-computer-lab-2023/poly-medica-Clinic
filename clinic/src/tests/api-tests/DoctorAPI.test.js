003619347f72089f6f8b7b0357a48b311b9441d2
AhmadHoseiny <ahmadshahhat9898@gmail.com>
2023-10-19 20:42:46 +0300

    setup mocking axios example

--- clinic/src/tests/api-tests/DoctorAPI.test.js
+++ clinic/src/tests/api-tests/DoctorAPI.test.js
@@ -7,9 +7,13 @@ import {
 import { OK_STATUS_CODE } from '../../utils/Constants.js';
 
 import DoctorModel from '../../database/models/Doctor.js';
+import AppointmentModel from '../../database/models/Appointment.js';
 import generateDoctor from '../model-generators/generateDoctor.js';
+import generateAppointment from '../model-generators/generateAppointment.js';
 
-import { describe, beforeEach, afterEach, expect, it } from '@jest/globals';
+import { describe, beforeEach, afterEach, expect, it, jest } from '@jest/globals';
+import { faker } from '@faker-js/faker';
+import axios from 'axios';
 
 describe('GET /doctor/:id', () => {
     
@@ -24,6 +28,57 @@ describe('GET /doctor/:id', () => {
 		const res = await request(app).get(`/doctor/${id}`);
 		expect(res.status).toBe(OK_STATUS_CODE);
 		expect(res._body.doctor._id).toBe(doctor.id);
+	});
+
+	afterEach(async () => {
+		await disconnectDBTest();
+	});
+});
+
+jest.mock('axios');
+describe('GET /doctors/:id/patients', () => {
+	
+	beforeEach(async () => {
+		await connectDBTest();
+	});
+	it('should return 200 OK and retrieve the patients correctly', async () => {
+		const firstDoctor = new DoctorModel(generateDoctor());
+		const secondDoctor = new DoctorModel(generateDoctor());
+		const len = faker.number.int({ min: 2, max: 10 });
+		const patients = [];
+		for(let i=0 ; i<len ; i++){
+			const patientId = faker.database.mongodbObjectId();
+			patients.push(patientId);
+			const appointment = new AppointmentModel(generateAppointment(patientId, firstDoctor._id));
+			await appointment.save();
+		}
+		const patientId = faker.database.mongodbObjectId();
+		patients.push(patientId);
+		const appointment = new AppointmentModel(generateAppointment(patientId, secondDoctor._id));
+		await appointment.save();
+
+		const axiosResponse = {
+			data: {
+				patients: patients.map((patient) => {
+					return {
+						_id: patient
+					};
+				})
+			}
+		};
+		
+		axios.get.mockResolvedValue(axiosResponse);
+		const res = await request(app).get(`/doctors/${firstDoctor._id}/patients`);
+		expect(res.status).toBe(OK_STATUS_CODE);
+		const retrievedPatientsIds = [];
+		res._body.finalListOFPatients.forEach((patient) => {
+			retrievedPatientsIds.push(patient._id);
+		});
+		expect(retrievedPatientsIds.length).toBe(len);
+		for(let i=0 ; i<len ; i++){
+			expect(retrievedPatientsIds.includes(patients[i])).toBe(true);
+		}
+		expect(retrievedPatientsIds.includes(patients[len])).toBe(false);
 	});
 
 	afterEach(async () => {
