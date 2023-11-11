5d8ac62de75b6b82c5062ea0735b553b815b33ba
Mohamed <mohamedhassan2082002@hotmail.com>
2023-11-11 19:27:35 +0200

    test getWalletAmmount endpoint

--- patient/src/tests/api-tests/PatientAPI.test.js
+++ patient/src/tests/api-tests/PatientAPI.test.js
@@ -5,13 +5,16 @@ import {
 	disconnectDBTest
 } from '../../utils/testing-utils.js';
 import { 
-	OK_STATUS_CODE
+	OK_STATUS_CODE,
+	NOT_FOUND_STATUS_CODE,
+	ERROR_STATUS_CODE
 } from '../../utils/Constants.js';
  
 
 import PatientModel from '../../database/models/Patient.js';
 import generatePatient from '../model-generators/generatePatient.js';
-
+import { faker } from '@faker-js/faker';
+ 
 import { describe, beforeEach, afterEach, expect, it } from '@jest/globals';
 
 
@@ -22,15 +25,31 @@ describe('GET /patients/:pateintId/wallet', () => {
 		await connectDBTest();
 	});
     
-	it('should return the wallet amount of the patient', async () => {
-		const patient = generatePatient();
-		const patientModel = new PatientModel(patient);
-		await patientModel.save();
-		const response = await request(app).get(`/patients/${patientModel._id}/wallet`);
-		console.log('rerer',response.body);
-		expect(response.status).toBe(OK_STATUS_CODE);
+	it('should return 200 OK and retrieve the patient wallet correctly', async () => {
+		const patient = new PatientModel(generatePatient());
+		await patient.save();
+		console.log('dsdsd',patient);
+       
+		const id = patient._id.toString();
+		console.log('dsdsd',patient);
+		const res = await request(app).get(`/patients/${id}/wallet`);
+		expect(res.status).toBe(OK_STATUS_CODE);
+                
 	});
 
+	it('should return 404 NOT FOUND when the patient is not found', async () => {   
+		const id = faker.database.mongodbObjectId();
+		const res = await request(app).get(`/patients/${id}/wallet`);
+		expect(res.status).toBe(NOT_FOUND_STATUS_CODE);
+	}
+	);
+
+	it('should return 500 ERROR when the id is invalid', async () => {
+		const id = faker.lorem.word();
+		const res = await request(app).get(`/patients/${id}/wallet`);
+		expect(res.status).toBe(ERROR_STATUS_CODE);
+	}
+	);
 	
     
 	
