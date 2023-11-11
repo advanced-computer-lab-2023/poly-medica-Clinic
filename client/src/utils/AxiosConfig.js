3adb93b4eaa69168dd68965b0891c883e037d950
Malek Mohamed Noureldean Elkssas <malekelkassas444@gmail.com>
2023-11-11 21:53:07 +0200

    comment the .interceptors.response ina axios config file

--- client/src/utils/AxiosConfig.js
+++ client/src/utils/AxiosConfig.js
@@ -11,64 +11,67 @@ const authenticationBaseUrl = 'http://localhost:8004';
 export const clinicAxios = axios.create({
     baseURL: clinicBaseUrl,
     withCredentials: true,
-}).interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    (error) => {
-      if (error.response && error.response.status === 401) {
-          Swal.fire({
-              icon: 'error',
-              title: 'Unauthorized',
-              text: 'You are unauthorized to access this resource.',
-            }).then(() => {
-              window.location.href = '/pages/login/login3';
-            });
-      }
-    }
-  );
+})
+// .interceptors.response.use(
+//     (response) => {
+//       return response;
+//     },
+//     (error) => {
+//       if (error.response && error.response.status === 401) {
+//           Swal.fire({
+//               icon: 'error',
+//               title: 'Unauthorized',
+//               text: 'You are unauthorized to access this resource.',
+//             }).then(() => {
+//               window.location.href = '/pages/login/login3';
+//             });
+//       } else return error
+//     }
+//   );
 
 // checkUserAuth(clinicAxios);
 
 export const patientAxios = axios.create({
     baseURL: patientBaseUrl,
     withCredentials: true,
-}).interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    (error) => {
-      if (error.response && error.response.status === 401) {
-          Swal.fire({
-              icon: 'error',
-              title: 'Unauthorized',
-              text: 'You are unauthorized to access this resource.',
-            }).then(() => {
-              window.location.href = '/pages/login/login3';
-            });
-      }
-    }
-  );
+})
+// .interceptors.response.use(
+//     (response) => {
+//       return response;
+//     },
+//     (error) => {
+//       if (error.response && error.response.status === 401) {
+//           Swal.fire({
+//               icon: 'error',
+//               title: 'Unauthorized',
+//               text: 'You are unauthorized to access this resource.',
+//             }).then(() => {
+//               window.location.href = '/pages/login/login3';
+//             });
+//       }
+//     }
+  // );
 
 export const pharmacyAxios = axios.create({
     baseURL: pharmacyBaseUrl,
     withCredentials: true,
-}).interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    (error) => {
-      if (error.response && error.response.status === 401) {
-          Swal.fire({
-              icon: 'error',
-              title: 'Unauthorized',
-              text: 'You are unauthorized to access this resource.',
-            }).then(() => {
-              window.location.href = '/pages/login/login3';
-            });
-      }
-    }
-  );
+})
+// .interceptors.response.use(
+//     (response) => {
+//       return response;
+//     },
+//     (error) => {
+//       if (error.response && error.response.status === 401) {
+//           Swal.fire({
+//               icon: 'error',
+//               title: 'Unauthorized',
+//               text: 'You are unauthorized to access this resource.',
+//             }).then(() => {
+//               window.location.href = '/pages/login/login3';
+//             });
+//       }
+//     }
+  // );
 
 export const authenticationAxios = axios.create({
     baseURL: authenticationBaseUrl,
