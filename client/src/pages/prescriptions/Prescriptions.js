2557d9728e3ebf556381e8c7bbdae98763ed8548
AhmadHoseiny <ahmadshahhat9898@gmail.com>
2023-11-14 00:24:40 +0200

    fix: handling frontend issues

--- client/src/pages/prescriptions/Prescriptions.js
+++ client/src/pages/prescriptions/Prescriptions.js
@@ -23,6 +23,7 @@ const Prescriptions = () => {
 		const getPrescriptions = async () => {
 			try {
 				const patientResponses = await patientAxios.get(`patient/${patientID}/prescriptions`);
+				console.log('patResp', patientResponses);
 				setPrescritpions(patientResponses.data);
 				setOriginalPrescritpions(patientResponses.data);
 				for (let i = 0; i < patientResponses.data.length; i++) {
