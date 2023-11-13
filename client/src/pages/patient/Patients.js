bb6b9d1fe66a0e1c8e8267e66278a731f224d4b5
amir-awad <amirawad004@gmail.com>
2023-11-13 20:31:25 +0200

    added loader and message when deleting patient

--- client/src/pages/patient/Patients.js
+++ client/src/pages/patient/Patients.js
@@ -12,16 +12,21 @@ import MainCard from 'ui-component/cards/MainCard';
 import PatientRow from './PatientRow';
 import DeleteConfirmationDialog from '../../ui-component/DeleteConfirmationDialog';
 import { clinicAxios, patientAxios } from '../../utils/AxiosConfig';
+import Message from '../../ui-component/Message';
 
 const Patients = () => {
 	const [patients, setPatients] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 	const [confirmDeleteDialogOpen, setConfirmDeleteDialogOpen] = useState(false);
 	const [patientToDelete, setPatientToDelete] = useState(null);
+	const [patientIsBeingDeleted, setPatientIsBeingDeleted] = useState(false);
+	const [patientDeleted, setPatientDeleted] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
 
 	useEffect(() => {
 		patientAxios
 			.get('/patients')
+			.then((response) => response.data)
 			.then((data) => {
 				setPatients(data.patients);
 				setIsLoading(false);
@@ -30,7 +35,7 @@ const Patients = () => {
 				console.error('Error fetching data:', error);
 				setIsLoading(false);
 			});
-	}, []);
+	}, [patients.length]);
 
 	const handleRemovePatient = (patientId) => {
 		setPatientToDelete(patientId);
@@ -38,19 +43,26 @@ const Patients = () => {
 	};
 
 	const handleConfirmDelete = () => {
+		setPatientIsBeingDeleted(true);
 		clinicAxios
 			.delete(`/patients/${patientToDelete}`)
-			.then(() =>
+			.then((response) => response.data)
+			.then(() => {
 				setPatients((prevPatients) =>
 					prevPatients.filter((patient) => patient._id !== patientToDelete),
-				),
-			)
-			.catch((error) => {
-				console.error('Error deleting patient:', error);
-			})
-			.finally(() => {
+				);
+				setPatientIsBeingDeleted(false);
 				setPatientToDelete(null);
 				setConfirmDeleteDialogOpen(false);
+				setPatientDeleted(true);
+				setTimeout(() => {
+					setPatientDeleted(false);
+				}, 2000);
+			})
+			.catch((error) => {
+				setPatientIsBeingDeleted(false);
+				setErrorMessage('Error deleting patient');
+				console.error('Error deleting patient:', error);
 			});
 	};
 
@@ -91,13 +103,24 @@ const Patients = () => {
 						</Table>
 					</TableContainer>
 
-					{/* Confirmation Dialog for Delete */}
+					{patientDeleted && (
+						<Message
+							message='Patient deleted successfully'
+							severity='success'
+							time={2000}
+							vertical={'bottom'}
+							horizontal={'right'}
+						/>
+					)}
+
 					<DeleteConfirmationDialog
 						open={confirmDeleteDialogOpen}
 						onClose={handleCancelDelete}
 						onConfirm={handleConfirmDelete}
 						title='Confirm Delete'
 						content='Are you sure you want to delete this patient?'
+						errorMessage={errorMessage}
+						someoneIsBeingDeleted={patientIsBeingDeleted}
 					/>
 				</div>
 			)}
