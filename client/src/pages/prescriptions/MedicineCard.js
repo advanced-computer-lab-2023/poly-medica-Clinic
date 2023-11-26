8034fe2634efc85dddf7e1c742733abe21649900
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2023-11-27 01:17:59 +0200

    frontend for adding medicine

--- client/src/pages/prescriptions/MedicineCard.js
+++ client/src/pages/prescriptions/MedicineCard.js
@@ -3,8 +3,9 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, IconButton } from '@mui/material';
 import { PHARMACY_BASE_URL } from '../../utils/Constants';
+import { Add } from '@mui/icons-material';
 export default function MedicineCard({ medicine }) {
     return (
         <Card sx={{ maxWidth: 250, margin: '2%', padding: '2%' }}>
@@ -14,15 +15,18 @@ export default function MedicineCard({ medicine }) {
                     height="140"
                     image={`${PHARMACY_BASE_URL}/medicines/${medicine._id}/pictures`}
                     alt="green iguana"
-                    sx={{ objectFit: 'contain', cursor:'default' }}
+                    sx={{ objectFit: 'contain', cursor: 'default' }}
                 />
-                <CardContent sx={{  cursor:'default' }}>
+                <CardContent sx={{ cursor: 'default' }}>
                     <Typography gutterBottom variant="h5" component="div">
                         {medicine.name}
                     </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Amount: {medicine.amount}
+                    <Typography sx={{ display: 'inline' }} variant="body2" color="text.secondary">
+                        {medicine.amount}
                     </Typography>
+                    <IconButton>
+                        <Add />
+                    </IconButton>
                 </CardContent>
             </CardActionArea>
         </Card>
--- client/src/pages/prescriptions/PrescriptionDetails.js
+++ client/src/pages/prescriptions/PrescriptionDetails.js
@@ -6,15 +6,17 @@ import {
 	Button,
 	Typography,
 	Grid,
+	IconButton,
 } from '@mui/material';
 import dayjs from 'dayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import MedicineCard from './MedicineCard';
+import { Add } from '@mui/icons-material';
 
 const PrescriptionDetails = ({ selectedPrescription, prescriptionDoctor, handleDialogClose, medicines }) => {
 	return (
-		
+
 		<Dialog open={selectedPrescription} onClose={handleDialogClose} PaperProps={{ sx: { minWidth: window.outerWidth > 800 ? 500 : 300 } }}  >
 			{selectedPrescription && prescriptionDoctor && (
 				<>
@@ -22,7 +24,7 @@ const PrescriptionDetails = ({ selectedPrescription, prescriptionDoctor, handleD
 					<DialogContent>
 						<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
 							<Typography variant="h4">{`Dr. ${prescriptionDoctor.userData.name}`}</Typography>
-							<Typography variant="body1">{`${prescriptionDoctor.specialty} Clinic`}</Typography>
+							<Typography variant="body1">{`${prescriptionDoctor.speciality} Clinic`}</Typography>
 						</div>
 						<Typography variant="subtitle1">Date:</Typography>
 						<Typography variant="body1">
@@ -39,10 +41,15 @@ const PrescriptionDetails = ({ selectedPrescription, prescriptionDoctor, handleD
 							medicines.length > 0 && (
 								<Grid container spacing={2}>
 									{medicines.map((medicine, index) => (
-										<Grid item xs={4} key={index}>
+										<Grid item xs={3} key={index}>
 											<MedicineCard medicine={medicine} />
 										</Grid>
 									))}
+									<Grid item xs={3}>
+										<IconButton title='Add medicine' color='primary' sx={{ height: '100%', width: '100%' }}>
+											<Add />
+										</IconButton>
+									</Grid>
 								</Grid>
 							)
 						}
