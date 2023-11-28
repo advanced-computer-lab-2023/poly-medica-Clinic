354477a2fcf9b903d1ae6d182ea4b1ac181f9b03
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2023-11-28 03:52:09 +0200

    add decrement medicine amount

--- client/src/pages/prescriptions/MedicineCard.js
+++ client/src/pages/prescriptions/MedicineCard.js
@@ -3,9 +3,10 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea, IconButton } from '@mui/material';
+import { CardActionArea, Grid, IconButton } from '@mui/material';
 import { PHARMACY_BASE_URL } from '../../utils/Constants';
 import { Add } from '@mui/icons-material';
+import { IconMinus } from '@tabler/icons';
 export default function MedicineCard({ medicine }) {
     return (
         <Card sx={{ maxWidth: 250, margin: '2%', padding: '2%' }}>
@@ -17,16 +18,30 @@ export default function MedicineCard({ medicine }) {
                     alt="green iguana"
                     sx={{ objectFit: 'contain', cursor: 'default' }}
                 />
-                <CardContent sx={{ cursor: 'default' }}>
+                <CardContent sx={{ cursor: 'default', textAlign: 'center' }}>
                     <Typography gutterBottom variant="h5" component="div">
                         {medicine.name}
                     </Typography>
-                    <Typography sx={{ display: 'inline' }} variant="body2" color="text.secondary">
-                        {medicine.amount}
-                    </Typography>
-                    <IconButton>
-                        <Add />
-                    </IconButton>
+                    <Grid container spacing={2} alignItems="center">
+                        <Grid item xs={4}>
+                            <IconButton>
+                                <IconMinus />
+                            </IconButton>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Typography color="text.secondary">
+                                {medicine.amount}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <IconButton>
+                                <Add />
+                            </IconButton>
+                        </Grid>
+                    </Grid>
+
+
+
                 </CardContent>
             </CardActionArea>
         </Card>
