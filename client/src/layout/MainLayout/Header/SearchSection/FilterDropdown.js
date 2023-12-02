126eb682c41b323a420fbb634471399806a5a1b3
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2023-12-02 02:58:35 +0200

    Resolve #42 and edit filter UI

--- client/src/layout/MainLayout/Header/SearchSection/FilterDropdown.js
+++ client/src/layout/MainLayout/Header/SearchSection/FilterDropdown.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import {
   Drawer,
   Select,
@@ -9,51 +9,67 @@ import {
   Card,
   Grid,
   TextField,
+  Slide,
 } from '@mui/material';
 
 const FilterDropdown = ({ filterData, onChange, isDrawerOpen, handleDrawerClose }) => {
-
   return (
-    <Drawer anchor="top" open={isDrawerOpen} onClose={handleDrawerClose}>
-      <Grid container spacing={2} sx={{ padding: '2%' }}>
-        {filterData.map((filter, index) => (
-          <Grid item xs={4} key={index}>
-            <Card variant="outlined" sx={{ p: 2 }}>
-              <Typography variant="subtitle1" sx={{ mb: 1 }}>
-                Filter By {filter.attribute}
+    <Drawer
+      anchor="right"
+      open={isDrawerOpen}
+      onClose={handleDrawerClose}
+      PaperProps={{ style: { height: '100%', overflow: 'hidden' } }}
+    >
+      <Slide direction="left" in={isDrawerOpen} mountOnEnter unmountOnExit>
+        {filterData.length > 0 ? (
+          <Grid container spacing={2} sx={{ padding: '4%', backgroundColor: '#f0f0f0', width: 300, height: '100%' }}>
+            <Grid item xs={12} textAlign={'center'}>
+              <Typography variant="h5" sx={{ marginBottom: '2%', color: '#333' }}>
+                Apply Filters on this page:
               </Typography>
-              {filter.attribute === 'Available Slots' ? (
-                <TextField
-                  type="datetime-local"
-                  value={filter.selectedValue}
-                  onChange={(event) => onChange(index, event.target.value)}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  fullWidth
-                />
-              ) : (
-                <FormControl fullWidth>
-                  <InputLabel>Select...</InputLabel>
-                  <Select
-                    value={filter.selectedValue}
-                    onChange={(event) => onChange(index, event.target.value)}
-                  >
-                    <MenuItem value="">
-                      <em>Select...</em>
-                    </MenuItem>
-                    {filter.values.map((value) => (
-                      <MenuItem key={value} value={value}>
-                        {value}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              )}
-            </Card>
+            </Grid>
+            {filterData.map((filter, index) => (
+              <Grid item xs={12} key={index}>
+                <Card variant="outlined" sx={{ p: 2, backgroundColor: '#fff', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}>
+                  <Typography variant="subtitle1" sx={{ mb: 1, color: '#555' }}>
+                    Filter By {filter.attribute}
+                  </Typography>
+                  {filter.attribute === 'Available Slots' ? (
+                    <TextField
+                      type="datetime-local"
+                      value={filter.selectedValue}
+                      onChange={(event) => onChange(index, event.target.value)}
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      fullWidth
+                      sx={{ mb: 2 }}
+                    />
+                  ) : (
+                    <FormControl fullWidth sx={{ mb: 2 }}>
+                      <InputLabel>Select...</InputLabel>
+                      <Select value={filter.selectedValue} onChange={(event) => onChange(index, event.target.value)}>
+                        <MenuItem value="">
+                          <em>Select...</em>
+                        </MenuItem>
+                        {filter.values.map((value) => (
+                          <MenuItem key={value} value={value}>
+                            {value}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                  )}
+                </Card>
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+        ) : (
+          <Typography variant="body2" sx={{ color: '#555', textAlign: 'center', padding: '2%' }}>
+            No Filter Data on this page
+          </Typography>
+        )}
+      </Slide>
     </Drawer>
   );
 };
