2633860cc329af66fb75cf133381819c1418ef4c
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2023-12-05 02:26:36 +0200

    remove import.meta

--- patient/src/utils/ImageUtils.js
+++ patient/src/utils/ImageUtils.js
@@ -1,6 +1,5 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { ZERO_INDEX, ONE } from './Constants.js';
 import fs from 'fs';
 const currentFilePath = getFileUrl();
 const __filename = fileURLToPath(currentFilePath);
@@ -10,10 +9,12 @@ function getFileUrl() {
 	const stackTraceFrames = String(new Error().stack)
 		.replace(/^Error.*\n/, '')
 		.split('\n');
-	const callerFrame = stackTraceFrames[ZERO_INDEX];
+	const callerFrame = stackTraceFrames[0];
 	let url = callerFrame.match(/\(([^)]+\.js)/);
-	if (url) url = url[ONE];
-	return url.startsWith('file:///') ? url.replace(/\\/g, '/') : 'file:///' + url.replace(/\\/g, '/');
+	if (url) url = url[1];
+	return url.startsWith('file:///')
+		? url.replace(/\\/g, '/')
+		: 'file:///' + url.replace(/\\/g, '/');
 }
 
 export const getImage = (folder, imageName) => {
@@ -31,5 +32,24 @@ export const deleteImage = (folder, imageName) => {
 		fs.unlinkSync(imagePathToDelete);
 	} catch (err) {
 		console.log('error in deleting task');
+	}
+};
+
+export const getFile = (folder, fileName) => {
+	try {
+		const filePath = path.join(__dirname, 'upload', folder, fileName);
+		return filePath;
+	} catch (error) {
+		console.log('error in getFile: ', error.message);
+	}
+};
+
+export const deleteFile = (folder, fileName) => {
+	try {
+		const filePathToDelete = path.join(__dirname, 'upload', folder, fileName);
+		console.log('filePathToDelete', filePathToDelete);
+		fs.unlinkSync(filePathToDelete);
+	} catch (err) {
+		throw new Error('error in deleting file');
 	}
 };
