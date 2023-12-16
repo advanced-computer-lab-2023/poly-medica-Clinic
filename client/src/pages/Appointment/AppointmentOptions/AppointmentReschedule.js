baea3f4e6082a1a7777d10797e365d829e3892bc
Abdelrahman Elmeky <65960126+Aelmeky@users.noreply.github.com>
2023-12-16 15:12:45 +0200

    adding the notification is appiontment reschule

--- client/src/pages/Appointment/AppointmentOptions/AppointmentReschedule.js
+++ client/src/pages/Appointment/AppointmentOptions/AppointmentReschedule.js
@@ -4,13 +4,17 @@ import { Typography } from '@mui/material';
 import AvailableSlotsList from './AvailableSlotsList.js';
 import Swal from 'sweetalert2';
 import '../../../assets/css/swalStyle.css';
-
-const AppointmentReschedule = ({ 
+import { communicationAxios } from 'utils/AxiosConfig.js';
+import { useUserContext } from 'hooks/useUserContext';
+import { PATIENT_TYPE_ENUM } from 'utils/Constants.js';
+const AppointmentReschedule = ({
     selectedAppointment,
     setSelectedAppointment,
     setTabValue,
     handleAppoinmentUpdate
 }) => {
+    const { user } = useUserContext();
+    const userIdToNotify = (user.type === PATIENT_TYPE_ENUM) ? selectedAppointment.doctorId : selectedAppointment.patientId;
     console.log('selectedAppointment', selectedAppointment);
     const [doctorAvailableSlots, setDoctorAvailableSlots] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
@@ -22,10 +26,10 @@ const AppointmentReschedule = ({
                 setIsLoading(false);
             })
             .catch((error) => {
-				console.log(error);
-			});
+                console.log(error);
+            });
     }
-    , []);
+        , []);
     const handleReschedule = async (availableSlotsIdx) => {
         clinicAxios
             .patch(`/appointments/reschedule/${selectedAppointment._id}`, {
@@ -33,42 +37,46 @@ const AppointmentReschedule = ({
                 availableSlotsIdx
             })
             .then((response) => {
-				Swal.fire(
-					'Appointment Rescheduled!',
-					'Your Appointment has been rescheduled successfully!',
-					'success',
-				)
-				.then(() => {
-					setTabValue('1');
-                    const updatedAppointment = response.data;
-                    setSelectedAppointment(updatedAppointment);
-                    handleAppoinmentUpdate(updatedAppointment);
-				});
-			})
-			.catch((error) => {
-				console.log(error);
-			});
+                Swal.fire(
+                    'Appointment Rescheduled!',
+                    'Your Appointment has been rescheduled successfully!',
+                    'success',
+                )
+                    .then(() => {
+                        setTabValue('1');
+                        const updatedAppointment = response.data;
+                        setSelectedAppointment(updatedAppointment);
+                        handleAppoinmentUpdate(updatedAppointment);
+                        communicationAxios.post(`/notification/${userIdToNotify}/type/appointment`, {
+                            notificationHead: 'Appointment Rescheduled',
+                            NotificationBody: 'the new time is '
+                        });
+                    });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
         console.log('availableSlotsIdx', availableSlotsIdx);
     };
     const handleConfirmation = (event) => {
         const availableSlotsIdx = parseInt(event.target.id);
         Swal.fire({
-			title: 'Confirm Rescheduling',
-			text: 'Are you sure you want to Reschedule this appointment?',
-			icon: 'question',
-			confirmButtonText: 'Yes',
-			showCancelButton: 'true',
-			cancelButtonText: 'No',
-		}).then(async (result) => {
-			if (result['isConfirmed']) {
-				await handleReschedule(availableSlotsIdx);
-			}
-		});
+            title: 'Confirm Rescheduling',
+            text: 'Are you sure you want to Reschedule this appointment?',
+            icon: 'question',
+            confirmButtonText: 'Yes',
+            showCancelButton: 'true',
+            cancelButtonText: 'No',
+        }).then(async (result) => {
+            if (result['isConfirmed']) {
+                await handleReschedule(availableSlotsIdx);
+            }
+        });
     };
     return (
-        <>   
+        <>
             {
-                isLoading 
+                isLoading
                 &&
                 <Typography>Loading...</Typography>
             }
