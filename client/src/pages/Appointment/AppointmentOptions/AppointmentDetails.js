8a5b8fb9347b363f968f0bc34d87b26f5772bfde
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2023-12-17 02:04:19 +0200

    merge appointment details

--- client/src/pages/Appointment/AppointmentOptions/AppointmentDetails.js
+++ client/src/pages/Appointment/AppointmentOptions/AppointmentDetails.js
@@ -17,6 +17,7 @@ import { useUserContext } from 'hooks/useUserContext.js';
 import { getDay, getTime } from '../../../utils/DateFormatter.js';
 import { patientCanRefund } from '../../../utils/AppointmentUtils.js';
 import AppointmentStatus from '../AppointmentStatus';
+import { useNavigate } from 'react-router-dom';
 
 const AppointmentDetails = ({
     selectedAppointment,
@@ -25,6 +26,8 @@ const AppointmentDetails = ({
 }) => {
     const { chats, setChats } = useChat();
     const { user } = useUserContext();
+    const navigate = useNavigate();
+
     const [cannotCompleteOrCancel, setCannotCompleteOrCancel] = useState(false);
     const handleCancel = async (refund) => {
         console.log('appointment cancelled, ', refund);
@@ -33,7 +36,7 @@ const AppointmentDetails = ({
             appointmentDate: selectedAppointment.date,
             refund,
         };
-        if(refund){
+        if (refund) {
             requestData.patientId = selectedAppointment.patientId;
             requestData.pricePaidByPatient = selectedAppointment.pricePaidByPatient;
             requestData.pricePaidToDoctor = selectedAppointment.pricePaidToDoctor;
@@ -46,12 +49,12 @@ const AppointmentDetails = ({
                     'Your Appointment has been cancelled successfully!',
                     'success',
                 )
-                .then(() => {
-                    console.log('response.data = ', response.data);
-                    const updatedAppointment = response.data;
-                    setSelectedAppointment(updatedAppointment);
-                    handleAppoinmentUpdate(updatedAppointment);
-                });
+                    .then(() => {
+                        console.log('response.data = ', response.data);
+                        const updatedAppointment = response.data;
+                        setSelectedAppointment(updatedAppointment);
+                        handleAppoinmentUpdate(updatedAppointment);
+                    });
             });
     };
     const handleComplete = async () => {
@@ -64,34 +67,34 @@ const AppointmentDetails = ({
                     'Appointment Completed!',
                     'Your Appointment has been completed successfully!',
                     'success',
-                    )
-                    
-                .then(() => {
-                    const app = response.data;
-                    setSelectedAppointment(app);
-                    handleAppoinmentUpdate(app);
-                    if (
-                        !chatExist(chats, app.patientId, app.doctorId) &&
-                        !chatExist(chats, app.doctorId, app.patientId)
-                    ) {
-                        const res = communicationAxios.post('/chat', {
-                            chat: {
-                                chatName: 'Doctor-Patient',
-                                users: [
-                                    {
-                                        id: app.patientId,
-                                        userType: PATIENT_TYPE_ENUM,
-                                    },
-                                    {
-                                        id: app.doctorId,
-                                        userType: DOCTOR_TYPE_ENUM,
-                                    },
-                                ],
-                            },
-                        });
-                        setChats([res.data, ...chats]);
-                    }
-                });
+                )
+
+                    .then(() => {
+                        const app = response.data;
+                        setSelectedAppointment(app);
+                        handleAppoinmentUpdate(app);
+                        if (
+                            !chatExist(chats, app.patientId, app.doctorId) &&
+                            !chatExist(chats, app.doctorId, app.patientId)
+                        ) {
+                            const res = communicationAxios.post('/chat', {
+                                chat: {
+                                    chatName: 'Doctor-Patient',
+                                    users: [
+                                        {
+                                            id: app.patientId,
+                                            userType: PATIENT_TYPE_ENUM,
+                                        },
+                                        {
+                                            id: app.doctorId,
+                                            userType: DOCTOR_TYPE_ENUM,
+                                        },
+                                    ],
+                                },
+                            });
+                            setChats([res.data, ...chats]);
+                        }
+                    });
             })
             .catch((error) => {
                 console.log(error);
@@ -100,65 +103,74 @@ const AppointmentDetails = ({
     const handleCancelConfirmation = () => {
         const refund = (user.type == 'doctor') || patientCanRefund(selectedAppointment.date);
         let swalText = '';
-        if(user.type == 'patient'){
-            swalText += (refund? 
+        if (user.type == 'patient') {
+            swalText += (refund ?
                 'The appointment reservation is refundable. ' :
-                'The appointment reservation is non-refundable. There are only 24 hours left for the appointment. '); 
+                'The appointment reservation is non-refundable. There are only 24 hours left for the appointment. ');
         }
-        else{ // user.type = 'doctor'
+        else { // user.type = 'doctor'
             swalText += 'The patient will be refunded the appointment reservation. ';
         }
         Swal.fire({
-			title: 'Do you Confirm Cancellation ?',
-			text: swalText,
-			icon: 'question',
-			confirmButtonText: 'Yes',
-			showCancelButton: 'true',
-			cancelButtonText: 'No',
-		}).then(async (result) => {
-			if (result['isConfirmed']) {
-				await handleCancel(refund);
-			}
-		});
+            title: 'Do you Confirm Cancellation ?',
+            text: swalText,
+            icon: 'question',
+            confirmButtonText: 'Yes',
+            showCancelButton: 'true',
+            cancelButtonText: 'No',
+        }).then(async (result) => {
+            if (result['isConfirmed']) {
+                await handleCancel(refund);
+            }
+        });
 
     };
     const handleCompleteConfirmation = () => {
         Swal.fire({
-			title: 'Confirm Completion',
-			text: 'Are you sure you want to complete this appointment?',
-			icon: 'question',
-			confirmButtonText: 'Yes',
-			showCancelButton: 'true',
-			cancelButtonText: 'No',
-		}).then(async (result) => {
-			if (result['isConfirmed']) {
-				await handleComplete();
-			}
-		});
+            title: 'Confirm Completion',
+            text: 'Are you sure you want to complete this appointment?',
+            icon: 'question',
+            confirmButtonText: 'Yes',
+            showCancelButton: 'true',
+            cancelButtonText: 'No',
+        }).then(async (result) => {
+            if (result['isConfirmed']) {
+                await handleComplete();
+            }
+        });
     };
     useEffect(() => {
         console.log('selectedAppointment == ', selectedAppointment);
-        if(selectedAppointment){
+        if (selectedAppointment) {
             setCannotCompleteOrCancel(
                 selectedAppointment.status.toUpperCase() == 'COMPLETE'
                 || selectedAppointment.status.toUpperCase() == 'CANCELLED'
             );
         }
     }, [selectedAppointment]);
-    let patientFamilyMember, familyMemberText;
-    if(selectedAppointment){
+    let patientFamilyMember, familyMemberText, runningAppointment;
+    if (selectedAppointment) {
+        const currentDate = new Date();
+        const appointmentDate = new Date(selectedAppointment.date);
+        const oneHourLater = new Date(appointmentDate.getTime() + 60 * 60 * 1000);
+        runningAppointment = currentDate >= appointmentDate && currentDate <= oneHourLater;
         patientFamilyMember = selectedAppointment.patientFamilyMember;
-        familyMemberText = (user.type=='patient')? 
+        familyMemberText = (user.type == 'patient') ?
             'your family member Mr/Miss ' :
             `Mr/Miss ${selectedAppointment.patientName}'s family member: Mr/Miss `;
     }
+
+    const handleAttendAppointment = () => {
+        navigate(`/${user.type}/pages/video-chat/${user.type === DOCTOR_TYPE_ENUM ?
+            selectedAppointment.patientId : selectedAppointment.doctorId}`);
+    };
     return (
         <>
             {selectedAppointment && (
                 <>
                     <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '1.5em' }}>
                         {
-                            user.type=='patient' 
+                            user.type == 'patient'
                             &&
                             <>
                                 <Typography variant='h4' >
@@ -167,7 +179,7 @@ const AppointmentDetails = ({
                             </>
                         }
                         {
-                            user.type=='doctor' 
+                            user.type == 'doctor'
                             &&
                             <>
                                 <Typography variant='h4'>
@@ -179,17 +191,17 @@ const AppointmentDetails = ({
 
                     <div style={{ display: 'flex', flexDirection: 'column', gap: '0.7em' }}>
                         <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
-                            <CalendarTodayIcon style={{ marginRight: '0.4em' }}/>
+                            <CalendarTodayIcon style={{ marginRight: '0.4em' }} />
                             <Typography variant='body1'>
                                 {`${getDay(selectedAppointment.date)} at ${getTime(selectedAppointment.date)}`}
                             </Typography>
                         </div>
                         <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
-                            <HelpCenterIcon style={{ marginRight: '0.4em' }}/>
+                            <HelpCenterIcon style={{ marginRight: '0.4em' }} />
                             <AppointmentStatus appointmentStatus={selectedAppointment.status} />
                         </div>
                         <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
-                            <StyleIcon style={{ marginRight: '0.4em' }}/>
+                            <StyleIcon style={{ marginRight: '0.4em' }} />
                             <Typography variant='body1'>
                                 {selectedAppointment.type}
                             </Typography>
@@ -198,15 +210,15 @@ const AppointmentDetails = ({
                             selectedAppointment.type == 'appointment'
                             &&
                             <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
-                                <AttachMoneyIcon style={{ marginRight: '0.4em' }}/>
+                                <AttachMoneyIcon style={{ marginRight: '0.4em' }} />
                                 {
-                                    user.type=='patient'
+                                    user.type == 'patient'
                                     &&
                                     <Typography variant='body1'>
                                         {`Paid $${selectedAppointment.pricePaidByPatient}`}
-                                    </Typography>                            }
+                                    </Typography>}
                                 {
-                                    user.type=='doctor'
+                                    user.type == 'doctor'
                                     &&
                                     <Typography variant='body1'>
                                         {`Received $${selectedAppointment.pricePaidToDoctor}`}
@@ -216,8 +228,8 @@ const AppointmentDetails = ({
                         }
 
                     </div>
-                    
-                    {  
+
+                    {
                         patientFamilyMember
                         &&
                         <>
@@ -232,23 +244,31 @@ const AppointmentDetails = ({
                         <Button
                             variant='contained'
                             color='error'
-                            sx = {{ marginTop: '3em', width: '15em' }}
+                            sx={{ marginTop: '3em', width: '15em' }}
                             onClick={handleCancelConfirmation}
                             disabled={cannotCompleteOrCancel}
                         >
                             Cancel Appointment
                         </Button>
                         {
-                            user.type=='doctor'
+                            user.type == 'doctor'
                             &&
                             <Button
                                 variant='contained'
                                 color='success'
-                                sx = {{ marginTop: '3em', width: '15em' }}
+                                sx={{ marginTop: '3em', width: '15em' }}
                                 onClick={handleCompleteConfirmation}
                                 disabled={cannotCompleteOrCancel}
                             >
                                 Complete Appointment
+                            </Button>
+                        }
+                        {(runningAppointment) &&
+                            <Button
+                                variant='contained'
+                                sx={{ marginTop: '3em', width: '15em' }}
+                                onClick={() => handleAttendAppointment()}>
+                                Attend Appointment
                             </Button>
                         }
                     </div>
