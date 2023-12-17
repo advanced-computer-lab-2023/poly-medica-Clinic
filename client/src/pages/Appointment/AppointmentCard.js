a2aa19b69035baa7ea2f7746045780ff81549895
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2023-12-17 03:55:16 +0200

    handle archived and empty medicines

--- client/src/pages/Appointment/AppointmentCard.js
+++ client/src/pages/Appointment/AppointmentCard.js
@@ -1,29 +1,34 @@
 import {
     ListItemButton,
     ListItemText,
-    ListItemAvatar
+    ListItemAvatar,
+    Tooltip
 } from '@mui/material';
 import TodayIcon from '@mui/icons-material/Today';
 import { getDay, getTime } from '../../utils/DateFormatter.js';
 import AppointmentStatus from './AppointmentStatus.js';
 
-const AppointmentCard = ({ appointment, setSelectedAppointment }) => {
+const AppointmentCard = ({ appointment, setSelectedAppointment, isRunning }) => {
     return (
-        <ListItemButton onClick={() => setSelectedAppointment(appointment)}>
-            <ListItemAvatar sx={{ paddingRight: '2%' }}>
-                <TodayIcon />
-            </ListItemAvatar>
-            <ListItemText
-                primary={`${getDay(appointment.date)}`}
-                secondary={`at ${getTime(appointment.date)}`}
-                sx={{
-                    width: '60%',
-                    lineHeight: '1.5em',
-                    maxHeight: '3em',
-                }}
-            />
-            <AppointmentStatus appointmentStatus={appointment.status} />
-        </ListItemButton>
+        <Tooltip title={isRunning ? 'Appointment is running' : ''}>
+            <ListItemButton
+                sx={{ background: isRunning ? '#F0E68C' : 'white' }}
+                onClick={() => setSelectedAppointment(appointment)}>
+                <ListItemAvatar sx={{ paddingRight: '2%' }}>
+                    <TodayIcon />
+                </ListItemAvatar>
+                <ListItemText
+                    primary={`${getDay(appointment.date)}`}
+                    secondary={`at ${getTime(appointment.date)}`}
+                    sx={{
+                        width: '60%',
+                        lineHeight: '1.5em',
+                        maxHeight: '3em',
+                    }}
+                />
+                <AppointmentStatus appointmentStatus={appointment.status} />
+            </ListItemButton>
+        </Tooltip>
     );
 };
 
