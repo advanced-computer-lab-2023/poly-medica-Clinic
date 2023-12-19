e82a16dd6ba16ab54518c55d3a73abb50548091b
amir-awad <amirawad004@gmail.com>
2023-12-19 20:18:11 +0200

    added initial kafka setup

--- clinic/start.js
+++ clinic/start.js
@@ -1,10 +1,10 @@
+import kafka from 'kafka-node';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import app from './app.js';
 import { PORT } from './src/utils/Constants.js';
 
 dotenv.config();
-
 
 const mongoURL = process.env.MONGO_URI;
 console.log(mongoURL);
@@ -20,6 +20,32 @@ const connect = async () => {
 
 await connect();
 
+const isRunning = () => {
+	const testModel = mongoose.model(
+		'test',
+		new mongoose.Schema({ name: String }),
+	);
+
+	const client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
+	console.log('Kafka client connected: ', client);
+
+	const consumer = new kafka.Consumer(client, [
+		{ topic: 'test', autoCommit: false },
+	]);
+
+	consumer.on('message', async (message) => {
+		console.log('Message received: ', message);
+		const test = await testModel.create({ name: message.value });
+		console.log('Test created: ', test);
+	});
+
+	consumer.on('error', (err) => {
+		console.log('Error: ', err);
+	});
+};
+
+const TIME = 3000;
+setTimeout(isRunning, TIME);
 
 const port = process.env.PORT || PORT;
 
